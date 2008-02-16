019a32cd4d3eb1aa5649d612ed4accb6dfb5133b
Michael Marineau <mike@marineau.org>
2008-02-16 12:35:10 -0800

    setup build for main program, based on test.c for now

---
+++ src/config.h
@@ -0,0 +1,7 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIDTH 800
+#define HEIGHT 480
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <gtk/gtk.h>
+#include "mtk/window.h"
+#include "mtk/clickarea.h"
+
+int main (int argc, char *argv[])
+{
+	GtkWidget *window;
+	GtkWidget *box;
+
+	gtk_init(&argc, &argv);
+
+	window = mtk_window_new(640, 480);
+	box = gtk_hbox_new(TRUE, 0);
+	gtk_container_add(GTK_CONTAINER(window), box);
+	gtk_box_pack_start(GTK_BOX(box), mtk_clickarea_new(), TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(box), mtk_clickarea_new(), TRUE, TRUE, 0);
+
+	gtk_widget_show_all(window);
+
+	gtk_main();
+
+	return 0;
+}

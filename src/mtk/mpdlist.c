627f4a9b09d186372bf37b8b8acfdba16f551de3
Michael Marineau <mike@marineau.org>
2008-03-20 13:04:57 -0700

    and yet another fix to fix the last fix

--- src/mtk/mpdlist.c
+++ src/mtk/mpdlist.c
@@ -229,7 +229,7 @@ static void mouse_release(mtk_widget_t *widget, int x, int y)
 		int pos = (float)(y - UNIT) / UNIT +
 			(float)mpdlist->scroll_top / UNIT;
 
-		if (pos >= mtk_list_length(mpdlist->list)) {
+		if (pos < mtk_list_length(mpdlist->list)) {
 			if (mpdlist->clicked(&mpdlist->data,
 			    mpdlist->list, pos)) {
 				mpdlist->timed_scroll = 0;
